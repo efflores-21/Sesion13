97d0b8bd345e25f99a095e1dcdfeeda4414801b2
Proman777 <cesardavidlaguna@gmail.com>
2024-09-30 10:13:07 -0600

    Sesion13

---
+++ Sesion13/MetodoBurbuja/models/Burbuja.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoBurbuja.models
+{
+    internal class Burbuja
+    {
+        public void BubbleSort(int[] array)
+        {
+            try
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[j] < array[i])
+                        {
+                            int temp = array[j];
+                            array[j] = array[i];
+                            array[i] = temp;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+        }
+
+        }
+    }
+
